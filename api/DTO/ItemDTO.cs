b7d78c258245660edeba8eae677040f4a9c8fb7a
alexanderhoyskel <1664AlfredAnna>
2025-10-13 20:22:39 +0200

    Update API start

---
+++ api/DTO/ItemDTO.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.DTO
+{
+    public class ItemDTO
+    {
+        public int ItemId { get; set; }
+
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ .,'\-]{2,20}", ErrorMessage = "Name must only contain letters, numbers and can be 2-20 characters long")]
+        [Display(Name = "Item name")]
+        public string Name { get; set; } = string.Empty;
+        
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description cannot be longer than 200 characters")]
+        public string? Description { get; set; }
+        
+        public string? ImageUrl { get; set; }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,43 +1,43 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyShop.DAL;
 using Serilog;
 using Serilog.Events;
-using MyShop.DAL;
-using Microsoft.AspNetCore.Builder;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ItemDbContext>(options => {
+    options.UseSqlite(builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);});
 
-builder.Services.AddDbContext<ItemDbContext>(options =>
-{
-    options.UseSqlite(
-        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
-});
-
-builder.Services.AddControllers();
 builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("CorsPolicy",
+        {
+            options.AddPolicy("CorsPolicy",
                 builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-    });
+        });
 
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 
-var loggerConfiguration = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.File($"APILogs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
 
-loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information() // levels: Trace< Information < Warning < Erorr < Fatal
+    .WriteTo.File($"../APILogs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log")
+    .Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
                             e.Level == LogEventLevel.Information &&
                             e.MessageTemplate.Text.Contains("Executed DbCommand"));
-
 var logger = loggerConfiguration.CreateLogger();
 builder.Logging.AddSerilog(logger);
-
 
 var app = builder.Build();
 
@@ -46,8 +46,8 @@ if (app.Environment.IsDevelopment())
     DBInit.Seed(app);
     app.UseSwagger();
     app.UseSwaggerUI();
+    
 }
-
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors("CorsPolicy");
